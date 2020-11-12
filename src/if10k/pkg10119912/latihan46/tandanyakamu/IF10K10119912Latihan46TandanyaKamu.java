b006a10f4c1b9a3c1ac309414427c2d64dc8a0e1
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-11-12 19:13:58 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan46/tandanyakamu/IF10K10119912Latihan46TandanyaKamu.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan46.tandanyakamu;
+import java.util.Scanner;
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * 
+ */
+public class IF10K10119912Latihan46TandanyaKamu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Age age = new Age(2018);
+
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Masukkan Tahun lahir anda : ");
+        age.setYearBirth(scn.nextInt());
+        
+        System.out.println("======Hasil Perhitungan Umur======");
+        System.out.println("Tahun Lahir Anda : " + age.getYearBirth(0));
+        System.out.println("Hari ini Tahun : " + age.getYearNow());
+        System.out.println("Umur kamu sampai hari ini adalah "
+                + age.hitungUmur()+ " tahun");
+        System.out.println("Tandanya Kamu : "+ age.tandanyaKamu(age.hitungUmur()));
+    }
+    
+}
